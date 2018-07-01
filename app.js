6f85a71e9e4b3db67e9c41fb0101c68729b172f0
MEGAN IVEY <printmeganivey@gmail.com>
2018-07-01 10:01:13 -0700

    Fixing local storage

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
-//variables
+//Global variables
 Products.productArray = [];
+var productNameArray = [];
+var chartDataVotes = [];
+var productArray = Products.productArray;
 var item1 = document.getElementById('item1');
 var item2 = document.getElementById('item2');
 var item3 = document.getElementById('item3');
@@ -10,7 +13,7 @@ var photoName2 = document.getElementById('photoName2');
 var photoName3 = document.getElementById('photoName3');
 var ulEl = document.getElementById('list');
 var button = document.getElementById('vote');
-//function for photos to choose from
+//Constructor function
 function Products(src, name, size) {
   this.src = src;
   this.name = name;
@@ -20,60 +23,53 @@ function Products(src, name, size) {
   Products.productArray.push(this);
 };
 Products.voteCount = 0;
-  new Products('./photos/bag.jpg', 'Bag', '100px');
-  new Products('./photos/bathroom.jpg', 'Bathroom', '100px');
-  new Products('./photos/banana.jpg', 'Banana', '100px');
-  new Products('./photos/boots.jpg', 'Boots', '100px');
-  new Products('./photos/breakfast.jpg', 'Breakfast', '100px');
-  new Products('./photos/bubblegum.jpg', 'Bubble Gum', '100px');
-  new Products('./photos/chair.jpg', 'Chair', '100px');
-  new Products('./photos/cthulhu.jpg', 'Cthulhu', '100px');
-  new Products('./photos/dog-duck.jpg', 'Dog Duck', '100px');
-  new Products('./photos/dragon.jpg', 'Dragon', '100px');
-  new Products('./photos/pen.jpg', 'Pen', '100px');
-  new Products('./photos/pet-sweep.jpg', 'Pet Sweep', '100px');
-  new Products('./photos/shark.jpg', 'Shark', '100px');
-  new Products('./photos/sweep.png', 'Sweep', '100px');
-  new Products('./photos/tauntaun.jpg', 'Tauntaun', '100px');
-  new Products('./photos/unicorn.jpg', 'Unicorn', '100px');
-  new Products('./photos/usb.gif', 'USB', '100px');
-  new Products('./photos/water-can.jpg', 'Water Can', '100px');
-  new Products('./photos/wine-glass.jpg', 'Wine Glass', '100px');
+//New Products
+new Products('./photos/bag.jpg', 'Bag', '100px');
+new Products('./photos/bathroom.jpg', 'Bathroom', '100px');
+new Products('./photos/banana.jpg', 'Banana', '100px');
+new Products('./photos/boots.jpg', 'Boots', '100px');
+new Products('./photos/breakfast.jpg', 'Breakfast', '100px');
+new Products('./photos/bubblegum.jpg', 'Bubble Gum', '100px');
+new Products('./photos/chair.jpg', 'Chair', '100px');
+new Products('./photos/cthulhu.jpg', 'Cthulhu', '100px');
+new Products('./photos/dog-duck.jpg', 'Dog Duck', '100px');
+new Products('./photos/dragon.jpg', 'Dragon', '100px');
+new Products('./photos/pen.jpg', 'Pen', '100px');
+new Products('./photos/pet-sweep.jpg', 'Pet Sweep', '100px');
+new Products('./photos/shark.jpg', 'Shark', '100px');
+new Products('./photos/sweep.png', 'Sweep', '100px');
+new Products('./photos/tauntaun.jpg', 'Tauntaun', '100px');
+new Products('./photos/unicorn.jpg', 'Unicorn', '100px');
+new Products('./photos/usb.gif', 'USB', '100px');
+new Products('./photos/water-can.jpg', 'Water Can', '100px');
+new Products('./photos/wine-glass.jpg', 'Wine Glass', '100px');
 
 //do while loops for random numbers for photos to change
 Products.random = function () {
   var randomNumber1 = 0;
   do {
     randomNumber1 = Math.floor(Math.random() * Products.productArray.length);
-
   } while (randomNumber1 === pastRandomNumber1 || randomNumber1 === pastRandomNumber2 || randomNumber1 === pastRandomNumber3);
-
   var randomNumber2 = 0;
   do {
     randomNumber2 = Math.floor(Math.random() * Products.productArray.length);
-
   } while (randomNumber2 === randomNumber1 || randomNumber2 === pastRandomNumber1 || randomNumber2 === pastRandomNumber2 || randomNumber2 === pastRandomNumber3);
-
-
   var randomNumber3 = 0;
   do {
     randomNumber3 = Math.floor(Math.random() * Products.productArray.length);
-
   } while (randomNumber3 === randomNumber1 || randomNumber3 === randomNumber2 || randomNumber3 === pastRandomNumber1 || randomNumber3 === pastRandomNumber2 || randomNumber3 === pastRandomNumber3);
 
-  item1.src = Products.productArray[randomNumber1].src;
-  item2.src = Products.productArray[randomNumber2].src;
-  item3.src = Products.productArray[randomNumber3].src;
+  item1.src = productArray[randomNumber1].src;
+  item2.src = productArray[randomNumber2].src;
+  item3.src = productArray[randomNumber3].src;
   //Times shown log
-  Products.productArray[randomNumber1].timesShown++;
-  Products.productArray[randomNumber2].timesShown++;
-  Products.productArray[randomNumber3].timesShown++;
+  productArray[randomNumber1].timesShown++;
+  productArray[randomNumber2].timesShown++;
+  productArray[randomNumber3].timesShown++;
 
-
-
-  photoName1.textContent = Products.productArray[randomNumber1].name;
-  photoName2.textContent = Products.productArray[randomNumber2].name;
-  photoName3.textContent = Products.productArray[randomNumber3].name;
+  photoName1.textContent = productArray[randomNumber1].name;
+  photoName2.textContent = productArray[randomNumber2].name;
+  photoName3.textContent = productArray[randomNumber3].name;
 
   item1.dataset.index = randomNumber1;
   item2.dataset.index = randomNumber2;
@@ -82,54 +78,33 @@ Products.random = function () {
   pastRandomNumber1 = randomNumber1;
   pastRandomNumber2 = randomNumber2;
   pastRandomNumber3 = randomNumber3;
-
 };
-
 var pastRandomNumber1 = -1;
 var pastRandomNumber2 = -2;
 var pastRandomNumber3 = -3;
-
 Products.random();
-
+//handle event function
 function handleClickEvent(event) {
   event.preventDefault();
   Products.voteCount++;
   Products.productArray[event.target.dataset.index].votes++;
-  storeClickData();
-  console.log(Products.productArray[event.target.dataset.index]);
   if (Products.voteCount == 25) {
-    renderList();
+    drawChart();
     click.removeEventListener('click', handleClickEvent);
     for (var j in Products.productArray) {
       chartDataVotes.push(Products.productArray[j].votes);
     }
     drawChart();
-    console.log(chartDataVotes);
-  }
-  if (localStorage.productArray) {
-    Product.all = JSON.parse(localStorage.productArray);
-  } else {
-    for (var i = 0; i < Products.name.length; i++) {
-      new Products(Products.names[i]);
-    }
-    console.log(Product.all);
   }
   Products.random();
+  localStorage.busmall = JSON.stringify(productArray);
 };
-
+//event listener
 click.addEventListener('click', handleClickEvent);
-
-var productNameArray = [];
-var chartDataVotes = [];
-
 for (var i in Products.productArray) {
   productNameArray.push(Products.productArray[i].name);
 }
-// console.log(productNameArray);
-
-
-
-// console.log(chartData);
+//Render Chart function
 function drawChart() {
   var ctx = document.getElementById("myChart").getContext('2d');
   var myChart = new Chart(ctx, {
@@ -192,4 +167,12 @@ function drawChart() {
       }
     }
   });
+  //Local storage
+  if (localStorage.productArray) {
+    productArray = JSON.parse(localStorage.productArray);
+  } else {
+    for (var i = 0; i < Products.name.length; i++) {
+      new Products(Products.name[i]);
+    }
+  }
 }
